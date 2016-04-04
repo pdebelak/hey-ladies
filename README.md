a52869bc39e0c97e7dd72cdfb45cf1db7cd9a3c8
pdebelak <pdebelak@gmail.com>
2016-04-03 20:00:02 -0500

    First crack at text-only replacement using tree walker

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hey Ladies
+
+Hey Ladies is a chrome extension that replaces the words guy and guys with the more gender neutral lady/ladies.
---
+++ index.js
@@ -0,0 +1,32 @@
+(function() {
+  'use strict';
+
+  const test = /guys?/i;
+  const regexes = [[/guys/g, 'ladies'], [/Guys/g, 'Ladies'], [/guys/gi, 'LADIES'], [/guy/g, 'lady'], [/Guy/g, 'Lady'], [/guy/gi, 'LADY']];
+  const regexLength = regexes.length;
+
+  function heyLadies() {
+    let n;
+
+    const html = document.querySelector('html');
+
+    if (html.textContent.match(test)) {
+      const walk = document.createTreeWalker(html, NodeFilter.SHOW_TEXT, null, false);
+      while (n = walk.nextNode()) {
+        let newContent = n.nodeValue;
+
+        if (newContent.match(test)) {
+          for (var i=0;i<regexLength;i++) {
+            newContent = newContent.replace(regexes[i][0], regexes[i][1]);
+          }
+
+          n.nodeValue = newContent;
+        }
+      }
+    }
+  }
+
+  heyLadies();
+
+  window.setInterval(heyLadies, 1000);
+})();
